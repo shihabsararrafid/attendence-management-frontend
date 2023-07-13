bba770c38ad0bcf02a2321ae4387854abc928812
shihabsararrafid <shrafid.532@gmail.com>
2023-07-14 01:15:29 +0600

    {done ] searching in the all classes enabling

--- lib/screens/teacher/allclasses.dart
+++ lib/screens/teacher/allclasses.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -32,6 +31,8 @@ class _AllClassesState extends State<AllClasses> {
   String? email = "none";
   String? id = "none";
   List<Course> courseList = [];
+
+  TextEditingController _searchController = TextEditingController();
   late Future<List<Course>> _allClassesFuture;
 
   Future<List<Course>> _allClasses() async {
@@ -59,6 +60,7 @@ class _AllClassesState extends State<AllClasses> {
           );
           courseList.add(course);
         }
+        print(courseList.length);
         return courseList;
       } else {
         throw Exception("Failed to Load Data");
@@ -100,6 +102,47 @@ class _AllClassesState extends State<AllClasses> {
     _allClassesFuture = _allClasses();
   }
 
+  List<Course> filteredList = [];
+
+  void filterClasses(String query) {
+    print(query);
+    filteredList = courseList
+        .where((course) =>
+            course.code.toLowerCase().contains(query.toLowerCase()) ||
+            course.section.toLowerCase().contains(query.toLowerCase()) ||
+            course.batchName.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    setState(() {
+      courseList = filteredList;
+      // Update the filtered list
+      // You can use this filteredList to display the filtered data in your UI
+    });
+  }
+
+  Future<void> deleteIt(String courseId) async {
+    final Uri deleteUri =
+        Uri.parse('http://192.168.0.113:4001/api/v1/teacher/course/$courseId');
+    final http.Response response = await http.delete(deleteUri);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        courseList.removeWhere((course) => course.id == courseId);
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Course deleted successfully'),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to delete course'),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -108,26 +151,42 @@ class _AllClassesState extends State<AllClasses> {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              SizedBox(
-                height: 50,
-              ),
+              SizedBox(height: 50),
               Text(
-                "All Classes",
+                'All Classes',
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
               ),
-              SizedBox(
-                height: 30,
+              SizedBox(height: 30),
+              TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  // Call a function to filter the courseList based on the search query
+                  filterClasses(value);
+                },
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: "Search",
+                  filled: true, //<-- SEE HERE
+                  fillColor: const Color(0xFFF1F3F5),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          width: 0, color: Color.fromARGB(255, 200, 209, 230)),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
               ),
+              SizedBox(height: 30),
               Center(
                 child: SizedBox(
-                  height: MediaQuery.of(context).size.height * .8,
                   child: FutureBuilder<List<Course>>(
                     future: _allClassesFuture,
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return ShimmerLoadingTable();
                       } else if (snapshot.hasData) {
-                        return BlueDataTable(courseList: snapshot.data!);
+                        return BlueDataTable(
+                          courseList: snapshot.data!,
+                          deleteCallback: deleteIt,
+                        );
                       } else if (snapshot.hasError) {
                         return Text('${snapshot.error}');
                       }
@@ -177,11 +236,8 @@ class ShimmerLoadingTable extends StatelessWidget {
       baseColor: Colors.grey[300]!,
       highlightColor: Colors.grey[100]!,
       child: DataTable(
-        columns: [
-          DataColumn(
-              label: Text(
-            'Course',
-          )),
+        columns: const [
+          DataColumn(label: Text('Course')),
           DataColumn(label: Text('Section')),
           DataColumn(label: Text('Series')),
           DataColumn(label: Text('Actions')),
@@ -216,8 +272,13 @@ class ShimmerLoadingTable extends StatelessWidget {
 
 class BlueDataTable extends StatelessWidget {
   final List<Course> courseList;
+  final Function(String) deleteCallback;
 
-  const BlueDataTable({required this.courseList, Key? key}) : super(key: key);
+  const BlueDataTable({
+    required this.courseList,
+    required this.deleteCallback,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -225,73 +286,86 @@ class BlueDataTable extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: DataTable(
         columnSpacing: 24,
-        columns: [
+        columns: const [
           DataColumn(
-              label: Text(
-            'Course',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16,
+            label: Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Course',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
             ),
-          )),
+          ),
           DataColumn(
-              label: Text('Section',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ))),
+            label: Text(
+              'Section',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
           DataColumn(
-              label: Text('Series',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ))),
+            label: Text(
+              'Series',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
           DataColumn(
-              label: Text('Actions',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ))),
+            label: Text(
+              'Actions',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
         ],
         rows: courseList.map((course) {
-          return DataRow(
-            cells: [
-              DataCell(
-                InkWell(
-                  onTap: () {
-                    // Handle course click
-                  },
-                  child: Center(child: Text(course.code)),
+          return DataRow(cells: [
+            DataCell(
+              InkWell(
+                onTap: () {
+                  // Handle course click
+                },
+                child: Center(
+                  child: Row(
+                    children: [
+                      Icon(Icons.arrow_forward, color: Colors.blue),
+                      Text(course.code),
+                    ],
+                  ),
                 ),
               ),
-              DataCell(
-                InkWell(
-                  onTap: () {
-                    // Handle section click
-                  },
-                  child: Center(child: Text(course.section)),
-                ),
+            ),
+            DataCell(
+              InkWell(
+                onTap: () {
+                  // Handle section click
+                },
+                child: Center(child: Text(course.section)),
               ),
-              DataCell(
-                InkWell(
-                  onTap: () {
-                    // Handle series click
-                  },
-                  child: Center(child: Text(course.batchName)),
-                ),
+            ),
+            DataCell(
+              InkWell(
+                onTap: () {
+                  // Handle series click
+                },
+                child: Center(child: Text(course.batchName)),
               ),
-              DataCell(
-                InkWell(
-                  onTap: () {
-                    // Handle series click
-                  },
-                  child: Center(
-                      child: const Text(
-                    "Delete",
+            ),
+            DataCell(
+              InkWell(
+                onTap: () {
+                  deleteCallback(course.id);
+                },
+                child: Center(
+                  child: const Text(
+                    'Delete',
                     style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.w400),
-                  )),
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ),
               ),
-            ],
-          );
+            ),
+          ]);
         }).toList(),
         dataRowHeight: 56,
         dividerThickness: 1.5,
